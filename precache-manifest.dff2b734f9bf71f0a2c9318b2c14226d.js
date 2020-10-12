3cf1c99f47ad0cdd0af25120c064d82640e8ae29
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-10-12 15:08:41 +0200

    Updates

---
+++ precache-manifest.dff2b734f9bf71f0a2c9318b2c14226d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837efda4492eb01844f3f1668ce022dd",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "3a50ac5f51db24251aff",
+    "url": "/burger-builder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d771155bac062f4f2236",
+    "url": "/burger-builder/static/js/2.bc2b25c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.bc2b25c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a50ac5f51db24251aff",
+    "url": "/burger-builder/static/js/main.45b62a32.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
